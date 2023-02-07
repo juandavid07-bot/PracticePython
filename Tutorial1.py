af37524583e40a67f572d53cdbf9635f571a0f18
Juan David Montenegro <juanea26a@gmail.com>
2023-02-07 17:08:29 -0500

    Commit inicial

---
+++ Tutorial1.py
@@ -0,0 +1,19 @@
+#Hola Mundo
+print("Ocho cinco")
+# Segunda forma de escribir una cadena de texto 
+print('Prueba 2')
+"""
+Este es un comentario 
+de linea de
+Prueba
+"""
+'''
+Esto tambien es un comentario 
+de linea de multi-linea 
+Prueba
+'''
+#Consultar el tipo de Dato
+print(type('Rikisimo')) #Tipo de dato -str-
+'''
+No se imprime el mensaje de hola python si no el tipo
+'''
