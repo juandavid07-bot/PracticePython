0842ef0498392cb32c20ba580d88cc89d7c83d1a
Juan David Montenegro <juanea26a@gmail.com>
2023-06-29 17:53:44 -0500

    Ejecisio 29/06/2023

---
+++ EjercicioPython.py
@@ -0,0 +1,10 @@
+#Escribir una función a la que se le pase una cadena <nombre> y muestre por 
+#pantalla el saludo ¡hola <nombre>!.
+
+def funcion(name):
+    print("hola "+name)
+
+
+print("Ingresa tu nombre")
+name=input()
+funcion(name)
